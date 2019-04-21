183c50a783033a9d13c17f76f361a4577328721d
ismael sidney <ismaelsidney@gmail.com>
2019-04-20 23:18:13 -0300

    app atualizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_API || 'http://localhost:3001'
+  baseURL:  'https://clonebox-backend.herokuapp.com/'
 });
 
 export default api;
